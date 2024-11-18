8807930a99ebba7daf520175fa16f2588b0fa03a
vferrerab <vferrerab01@iescastelar.com>
2024-11-18 09:50:43 +0100

    Add files via upload

---
+++ ProcesosPorLotes/mkprofile.bat
@@ -0,0 +1,13 @@
+@echo off
+REM Script de prueba
+REM Autor: Victor
+REM Fecha: 18/11/24
+
+mkdir %USERPROFILE%\asir %USERPROFILE%\dam %USERPROFILE%\smr %USERPROFILE%\daw %USERPROFILE%\ceti
+
+mkdir %USERPROFILE%\asir\docs %USERPROFILE%\asir\pract %USERPROFILE%\asir\exam
+mkdir %USERPROFILE%\dam\docs %USERPROFILE%\dam\pract %USERPROFILE%\dam\exam
+mkdir %USERPROFILE%\daw\docs %USERPROFILE%\daw\pract %USERPROFILE%\daw\exam
+mkdir %USERPROFILE%\smr\docs %USERPROFILE%\smr\pract %USERPROFILE%\smr\exam
+mkdir %USERPROFILE%\ceti\docs %USERPROFILE%\ceti\pract %USERPROFILE%\ceti\exam
+
