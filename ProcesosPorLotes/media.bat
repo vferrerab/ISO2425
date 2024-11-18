d329dcbf030359ce0beab67a16d27907fff802b6
vferrerab <vferrerab01@iescastelar.com>
2024-11-18 10:25:02 +0100

    Add files via upload

---
+++ ProcesosPorLotes/media.bat
@@ -0,0 +1,14 @@
+@echo off
+REM Script de prueba
+REM Autor: Victor
+REM Fecha: 18/11/24
+
+set num1=4
+set num2=6
+set num3=5
+set nummedia=3
+set /A suma=%num1% + %num2% + %num3%
+set /A media=%suma% / %nummedia%
+
+
+echo La media de los tres numeros %num1% %num2% %num3% es %media%
