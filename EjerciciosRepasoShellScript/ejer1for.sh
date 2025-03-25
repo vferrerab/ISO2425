9f300d2be8a4ba695547ba656818803197f3732a
vferrerab <vferrerab01@iescastelar.com>
2025-03-25 10:17:18 +0100

    Delete EjerciciosRepasoShellScript directory

--- EjerciciosRepasoShellScript/ejer1for.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#Victor Ferrera Brioa
-
-
-for $borrar in paco juan luis maria felipe sara; do
-
-	rm -rf /home/$borrar/*.bak
-
-done
--- EjerciciosRepasoShellScript/ejercicio1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Ejercici1 
-# Autor: Victor Ferrera Brioa
-# Fecha: 20/01/2025
-
-clear
-echo "Hola $1"
-
-
